7600182b5bc187a7da1d4e27fb493734ae247357
Chewsterchew <chew@chew.pw>
2018-06-07 19:46:37 -0500

    EValuate RUBY Expressiosn

---
+++ lib/plugins/eval.rb
@@ -0,0 +1,22 @@
+module Eval
+  extend Discordrb::Commands::CommandContainer
+
+  command(:eval) do |event, *code|
+    break unless event.user.id == CONFIG['user_id']
+    begin
+      event.channel.send_embed do |e|
+        e.title = '**Evaluated Successfully**'
+
+        e.description = eval code.join(' ')
+        e.color = '00FF00'
+      end
+    rescue StandardError => f
+      event.channel.send_embed do |e|
+        e.title = '**Evaluation Failed!**'
+
+        e.description = f.to_s
+        e.color = 'FF0000'
+      end
+    end
+  end
+end
