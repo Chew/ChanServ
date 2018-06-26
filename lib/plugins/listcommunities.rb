a663efa43f03b3de900b5d8e7034de75b54cbe3c
Chewsterchew <chew@chew.pw>
2018-06-26 15:14:34 -0500

    2 -> 3

--- lib/plugins/listcommunities.rb
+++ lib/plugins/listcommunities.rb
@@ -6,15 +6,15 @@ module ListCommunities
     event.channel.send_embed do |e|
       e.title = "Communities Available on #{event.server.name}"
 
-      e.add_field(name: 'Gaming', value: "**Members**: #{bot.channel(424_379_734_483_533_845).permission_overwrites.count - 2}\n**Join**: `;j gaming`", inline: true)
-      e.add_field(name: 'Crypto', value: "**Members**: #{bot.channel(424_379_772_219_424_779).permission_overwrites.count - 2}\n**Join**: `;j crypto`", inline: true)
-      e.add_field(name: 'Programming', value: "**Members**: #{bot.channel(424_379_786_006_364_161).permission_overwrites.count - 2}\n**Join**: `;j programming`", inline: true)
-      e.add_field(name: 'Tech', value: "**Members**: #{bot.channel(424_379_920_924_278_784).permission_overwrites.count - 2}\n**Join**: `;j tech`", inline: true)
-      e.add_field(name: 'Music', value: "**Members**: #{bot.channel(424_379_929_539_641_345).permission_overwrites.count - 2}\n**Join**: `;j music`", inline: true)
-      e.add_field(name: 'Pets', value: "**Members**: #{bot.channel(424_379_940_604_084_224).permission_overwrites.count - 2}\n**Join**: `;j pets`", inline: true)
-      e.add_field(name: 'Memes', value: "**Members**: #{bot.channel(424_379_961_256_968_192).permission_overwrites.count - 2}\n**Join**: `;j memes`", inline: true)
-      e.add_field(name: 'LGBT', value: "**Members**: #{bot.channel(424_663_031_482_679_316).permission_overwrites.count - 2}\n**Join**: `;j lgbt`", inline: true)
-      e.add_field(name: 'Anime', value: "**Members**: #{bot.channel(425_422_889_374_842_891).permission_overwrites.count - 2}\n**Join**: `;j anime`", inline: true)
+      e.add_field(name: 'Gaming', value: "**Members**: #{bot.channel(424_379_734_483_533_845).permission_overwrites.count - 3}\n**Join**: `;j gaming`", inline: true)
+      e.add_field(name: 'Crypto', value: "**Members**: #{bot.channel(424_379_772_219_424_779).permission_overwrites.count - 3}\n**Join**: `;j crypto`", inline: true)
+      e.add_field(name: 'Programming', value: "**Members**: #{bot.channel(424_379_786_006_364_161).permission_overwrites.count - 3}\n**Join**: `;j programming`", inline: true)
+      e.add_field(name: 'Tech', value: "**Members**: #{bot.channel(424_379_920_924_278_784).permission_overwrites.count - 3}\n**Join**: `;j tech`", inline: true)
+      e.add_field(name: 'Music', value: "**Members**: #{bot.channel(424_379_929_539_641_345).permission_overwrites.count - 3}\n**Join**: `;j music`", inline: true)
+      e.add_field(name: 'Pets', value: "**Members**: #{bot.channel(424_379_940_604_084_224).permission_overwrites.count - 3}\n**Join**: `;j pets`", inline: true)
+      e.add_field(name: 'Memes', value: "**Members**: #{bot.channel(424_379_961_256_968_192).permission_overwrites.count - 3}\n**Join**: `;j memes`", inline: true)
+      e.add_field(name: 'LGBT', value: "**Members**: #{bot.channel(424_663_031_482_679_316).permission_overwrites.count - 3}\n**Join**: `;j lgbt`", inline: true)
+      e.add_field(name: 'Anime', value: "**Members**: #{bot.channel(425_422_889_374_842_891).permission_overwrites.count - 3}\n**Join**: `;j anime`", inline: true)
 
       e.color = '00FF00'
     end
