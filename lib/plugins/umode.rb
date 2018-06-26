87328b771e66afa4aed25e06ee7172c69bd00020
Chewsterchew <chew@chew.pw>
2018-06-26 15:20:37 -0500

    add the umode command woke

---
+++ lib/plugins/umode.rb
@@ -0,0 +1,12 @@
+module UMode
+  extend Discordrb::Commands::CommandContainer
+
+  command(%i[umodes umode]) do |event|
+    modez = modes(event).join('')
+    if modez.length.positive?
+      event.respond 'Your user mode is: +' + modez
+    else
+      event.respond 'You have no user modes.'
+    end
+  end
+end
