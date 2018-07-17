cb694f44d7b5a531cee872b825816e7e1f299bd7
Chewsterchew <chew@chew.pw>
2018-07-16 21:15:17 -0500

    no swear

--- lib/chanserv.rb
+++ lib/chanserv.rb
@@ -100,5 +100,31 @@ Bot.message(includes: 'discord.gg') do |event|
   end
 end
 
+Bot.message(includes: '') do |event|
+  next if %w[Oper Owner Admin Op].include? role(event).to_s
+  next unless [134_445_052_805_120_001, 235_175_875_732_176_896, 421_472_240_169_910_282].include? event.channel.id.to_i
+  message = event.message.to_s
+  message.downcase!
+  swears = File.readlines('swears.txt') { |line| line.split.map(&:to_s).join }
+  swears = swears[0]
+  swears.delete!("\n")
+  swears = swears.split(',')
+  swears.each do |swear|
+    next unless message.include?(swear)
+    event.message.delete
+    event.send_temporary_message("#{event.user.mention}, please do not swear!", 10)
+    bot.channel(424_005_662_113_136_640).send_embed do |e|
+      e.title = 'Someone just swore!'
+
+      e.add_field(name: 'Invoker', value: event.user.mention, inline: true)
+      e.add_field(name: 'Swear', value: swear, inline: true)
+      e.add_field(name: 'Channel', value: "<##{event.channel.id}>", inline: true)
+      e.add_field(name: 'Message', value: message, inline: false)
+
+      e.color = 'FF0000'
+    end
+  end
+end
+
 puts 'Bot is ready!'
 Bot.run
