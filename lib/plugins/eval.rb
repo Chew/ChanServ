6cd5c0243165a7676608c3eabba5ef41d3536cf0
Chew <chew@chew.pw>
2020-08-17 15:13:08 -0500

    Eval improvements

--- lib/plugins/eval.rb
+++ lib/plugins/eval.rb
@@ -3,6 +3,7 @@ module Eval
 
   command(:eval) do |event, *code|
     break unless event.user.id == CONFIG['owner_id']
+
     begin
       event.channel.send_embed do |e|
         e.title = '**Evaluated Successfully**'
@@ -10,11 +11,18 @@ module Eval
         e.description = eval code.join(' ')
         e.color = '00FF00'
       end
+    rescue RestClient::BadRequest => f
+      event.channel.send_embed do |e|
+        e.title = '**Evaluation Failed!**'
+
+        e.description = f.response.body
+        e.color = 'FF0000'
+      end
     rescue StandardError => f
       event.channel.send_embed do |e|
         e.title = '**Evaluation Failed!**'
 
-        e.description = f.to_s
+        e.description = f.message
         e.color = 'FF0000'
       end
     end
