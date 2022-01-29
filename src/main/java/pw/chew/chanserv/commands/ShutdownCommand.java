ba2d86aa8a5adadb5b13884ea64eb2388a606780
Chew <chew@chew.pw>
2022-01-29 00:25:20 -0600

    Pre-start gradle daemon on shutdown

--- src/main/java/pw/chew/chanserv/commands/ShutdownCommand.java
+++ src/main/java/pw/chew/chanserv/commands/ShutdownCommand.java
@@ -4,9 +4,12 @@ import com.jagrosh.jdautilities.command.SlashCommand;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.internal.utils.Checks;
+import org.slf4j.LoggerFactory;
 import pw.chew.chanserv.util.Roles;
 import pw.chew.jdachewtils.command.OptionHelper;
 
+import java.io.IOException;
 import java.util.Collections;
 
 public class ShutdownCommand extends SlashCommand {
@@ -22,15 +25,34 @@ public class ShutdownCommand extends SlashCommand {
     }
 
     @Override
-    protected void execute(SlashCommandEvent commandEvent) {
-        // whee
-        boolean shouldRemove = OptionHelper.optBoolean(commandEvent, "remove_slash_commands", false);
-        commandEvent.reply("Shutting down....").setEphemeral(true).queue((msg) -> {
-            if (shouldRemove) {
-                commandEvent.getGuild().updateCommands().queue();
+    protected void execute(SlashCommandEvent event) {
+        event.reply("Pre-starting gradle daemon...").setEphemeral(true).queue(interactionHook -> {
+            // Run `./gradlew help` in the command line and wait for exit code 0
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.directory(null);
+            processBuilder.command("/bin/bash", "-c", "./gradlew help");
+
+            try {
+                Process process = processBuilder.start();
+                int exitVal = process.waitFor();
+                if (exitVal > 0) {
+                    LoggerFactory.getLogger(ShutdownCommand.class).warn("Error starting gradle daemon! " + exitVal);
+                }
+                LoggerFactory.getLogger(ShutdownCommand.class).debug("Gradle daemon started with exit code " + exitVal);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
             }
 
-            commandEvent.getJDA().shutdown();
+            // whee
+            boolean shouldRemove = OptionHelper.optBoolean(event, "remove_slash_commands", false);
+            interactionHook.editOriginal("Shutting down....").queue((msg) -> {
+                if (shouldRemove) {
+                    Checks.notNull(event.getGuild(), "Server");
+                    event.getGuild().updateCommands().queue();
+                }
+
+                event.getJDA().shutdown();
+            });
         });
     }
 }
