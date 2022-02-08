57f3db60c9bff02dfaf6538d67483eb55d1addbb
Chew <chew@chew.pw>
2022-02-07 20:59:01 -0600

    Better random and add a reason when changing role color

--- src/main/java/pw/chew/chanserv/commands/RandomColorCommand.java
+++ src/main/java/pw/chew/chanserv/commands/RandomColorCommand.java
@@ -3,11 +3,13 @@ package pw.chew.chanserv.commands;
 import com.jagrosh.jdautilities.command.SlashCommand;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.internal.utils.Checks;
 
 import java.util.List;
-import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class RandomColorCommand extends SlashCommand {
+    private static final RandomGenerator RANDOM = RandomGenerator.getDefault();
 
     public RandomColorCommand() {
         this.name = "randomcolor";
@@ -18,19 +20,20 @@ public class RandomColorCommand extends SlashCommand {
 
     @Override
     protected void execute(SlashCommandEvent event) {
+        Checks.notNull(event.getGuild(), "Guild"); // Slash commands are server-specific
         List<Role> role = event.getGuild().getRolesByName(event.getUser().getId(), true);
-        Random r = new Random();
-        int color = r.nextInt((16777215) + 1);
+        int color = RANDOM.nextInt((16777215) + 1);
         Role current;
         String oldColor;
         if (role.isEmpty()) {
-            current = event.getGuild().createRole().setName(event.getUser().getId()).setColor(color).complete();
+            current = event.getGuild().createRole().setName(event.getUser().getId()).setColor(color)
+                .reason("Creating color role for " + event.getUser().getAsTag()).complete();
             oldColor = "absolutely nothing";
             event.getGuild().modifyRolePositions(false).selectPosition(current).moveTo(4).complete();
         } else {
             current = role.get(0);
             oldColor = "#" + Integer.toHexString(current.getColor().getRGB()).substring(2);
-            current.getManager().setColor(color).complete();
+            current.getManager().setColor(color).reason("Updating color role for " + event.getUser().getAsTag()).complete();
         }
         if (!event.getMember().getRoles().contains(current)) {
             event.getGuild().addRoleToMember(event.getMember(), current).complete();
