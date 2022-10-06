9c1420c5865f8727d0338b9b84b3b53aeaf214b8
Chew <chew@chew.pw>
2022-10-06 17:26:53 -0500

    Cache Channel command

---
+++ src/main/java/pw/chew/chanserv/commands/CacheChannelCommand.java
@@ -0,0 +1,66 @@
+package pw.chew.chanserv.commands;
+
+import com.jagrosh.jdautilities.command.SlashCommand;
+import com.jagrosh.jdautilities.command.SlashCommandEvent;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import pw.chew.chanserv.listeners.MessageModificationHandler;
+import pw.chew.chanserv.objects.FanclubMessage;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CacheChannelCommand extends SlashCommand {
+    public CacheChannelCommand() {
+        this.name = "cache";
+        this.help = "Wipes then caches a channel's messages.";
+        this.guildOnly = true;
+        this.userPermissions = new Permission[]{Permission.MANAGE_SERVER};
+        this.options = Arrays.asList(
+            new OptionData(OptionType.CHANNEL, "channel", "The channel to cache messages from")
+                .setChannelTypes(ChannelType.TEXT).setRequired(true),
+            new OptionData(OptionType.INTEGER, "amount", "The amount of messages to cache")
+                .setMinValue(1).setRequired(true)
+        );
+    }
+
+    @Override
+    protected void execute(SlashCommandEvent event) {
+        GuildChannelUnion channel = event.getOption("channel", OptionMapping::getAsChannel);
+        if (channel == null) {
+            event.reply("You must specify a channel!").setEphemeral(true).queue();
+            return;
+        }
+
+        event.deferReply(true).queue(interactionHook -> {
+
+            // Get message cache
+            var cache = MessageModificationHandler.getCache();
+
+            for (FanclubMessage message : cache.values()) {
+                if (message.channelId().equals(channel.getId())) {
+                    cache.remove(message.id());
+                }
+            }
+
+            channel.asGuildMessageChannel().getIterableHistory()
+                .takeAsync(event.getOption("amount", 0, OptionMapping::getAsInt)) // Collect 1000 messages
+                .thenApply(list ->
+                    list.stream()
+                        .map(FanclubMessage::new)
+                        .collect(Collectors.toList())
+                ).thenAccept(messages -> {
+                    // Add messages to cache
+                    for (FanclubMessage message : messages) {
+                        cache.put(message.id(), message);
+                    }
+
+                    event.getHook().sendMessage("Cached " + messages.size() + " messages from " + channel.getName() + "!").queue();
+                });
+        });
+    }
+}
