150864db8ceb339191eb41dcc07e9ed256f55f9f
Chew <chew@chew.pw>
2022-10-30 11:46:41 -0500

    Update to JDA 5.0.0-alpha.22

--- src/main/java/pw/chew/chanserv/listeners/ReadyHandler.java
+++ src/main/java/pw/chew/chanserv/listeners/ReadyHandler.java
@@ -1,7 +1,7 @@
 package pw.chew.chanserv.listeners;
 
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.slf4j.LoggerFactory;
 
