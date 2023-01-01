792a0708d1254430160c2f96ac9cbf41685522e3
Chew <chew@chew.pw>
2023-01-01 10:58:53 -0600

    Also support lorelai reactions

--- src/main/java/pw/chew/chanserv/listeners/RoryListener.java
+++ src/main/java/pw/chew/chanserv/listeners/RoryListener.java
@@ -3,7 +3,6 @@ package pw.chew.chanserv.listeners;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.entities.emoji.RichCustomEmoji;
-import net.dv8tion.jda.api.events.message.GenericMessageEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -18,9 +17,7 @@ public class RoryListener extends ListenerAdapter {
         // Ensure this is the rory fanclub
         if (!event.isFromGuild() || !event.getGuild().getId().equals("134445052805120001")) return;
 
-        if (event.getMessage().getContentRaw().toLowerCase(Locale.ROOT).contains("rory")) {
-            addReaction(event.getMessage(), event);
-        }
+        this.handleMessage(event.getMessage());
     }
 
     @Override
@@ -28,21 +25,30 @@ public class RoryListener extends ListenerAdapter {
         // Ensure this is the rory fanclub
         if (!event.isFromGuild() || !event.getGuild().getId().equals("134445052805120001")) return;
 
-        if (event.getMessage().getContentRaw().toLowerCase(Locale.ROOT).contains("rory")) {
-            addReaction(event.getMessage(), event);
+        this.handleMessage(event.getMessage());
+    }
+
+    /**
+     * Handles a Rory Fanclub message
+     *
+     * @param msg the message to handle
+     */
+    public void handleMessage(Message msg) {
+        String message = msg.getContentRaw().toLowerCase(Locale.ROOT);
+        if (message.contains("rory")) {
+            addReaction(msg, "rory");
+        } else if (message.contains("lory") || message.contains("lorelai")) {
+            addReaction(msg, "lory");
         }
     }
 
     /**
      * Adds a Rory reaction to the message
      * @param message The message
-     * @param event The event to get the server from
      */
-    public void addReaction(Message message, GenericMessageEvent event) {
-        // Ensure this is the rory fanclub
-        if (!event.isFromGuild() || !event.getGuild().getId().equals("134445052805120001")) return;
+    public void addReaction(Message message, String reaction) {
         // Get all emotes
-        List<RichCustomEmoji> emotes = event.getGuild().getEmojiCache().stream().filter(emote -> emote.getName().toLowerCase(Locale.ROOT).contains("rory")).toList();
+        List<RichCustomEmoji> emotes = message.getGuild().getEmojiCache().stream().filter(emote -> emote.getName().toLowerCase(Locale.ROOT).contains(reaction)).toList();
 
         // Get a random emote from the list using a random number generator
         int random = (int) (Math.random() * emotes.size());
