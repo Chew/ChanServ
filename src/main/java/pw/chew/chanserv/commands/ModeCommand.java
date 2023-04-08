91034ffda7b89712a503e9f7edee9f140c34bbf0
Chew <chew@chew.pw>
2023-04-07 22:22:12 -0500

    Use autocomplete for /mode

--- src/main/java/pw/chew/chanserv/commands/ModeCommand.java
+++ src/main/java/pw/chew/chanserv/commands/ModeCommand.java
@@ -4,6 +4,8 @@ import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.internal.utils.Checks;
@@ -13,7 +15,9 @@ import pw.chew.chanserv.util.Roles;
 import pw.chew.chewbotcca.util.ResponseHelper;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ModeCommand extends SlashCommand {
 
@@ -21,17 +25,9 @@ public class ModeCommand extends SlashCommand {
         this.name = "mode";
         this.help = "Change a specified user's modes (requires Admin+)";
 
-        OptionData modes = new OptionData(OptionType.STRING, "mode", "The mode to give.").setRequired(true);
-        for (Roles.UserMode mode : Roles.UserMode.values()) {
-            if (mode.canGive()) {
-                modes.addChoice("+" + mode.name(),"+" + mode.name());
-                modes.addChoice("-" + mode.name(),"-" + mode.name());
-            }
-        }
-
         this.options = Arrays.asList(
-            new OptionData(OptionType.USER, "user", "The user to promote to voiced.").setRequired(true),
-            modes
+            new OptionData(OptionType.USER, "user", "The user to promote to voiced.", true),
+            new OptionData(OptionType.STRING, "mode", "The mode to give.", true, true)
         );
     }
 
@@ -49,37 +45,101 @@ public class ModeCommand extends SlashCommand {
             return;
         }
 
-        if (mode.length() > 2) {
-            boolean add = mode.charAt(0) == '+';
-            String modetemp = mode.substring(1);
-            String[] modes = modetemp.split("");
-            if (add) {
-                for (String theMode : modes) {
-                    Roles.UserMode to_add = Roles.UserMode.valueOf(theMode);
-                    to_add.assignMode(user);
-                }
-            } else {
-                for (String theMode : modes) {
-                    Roles.UserMode to_add = Roles.UserMode.valueOf(theMode);
-                    to_add.removeMode(user);
-                }
+        // Figure out what the current input is
+        // We have an input such as +abc-xyz
+        // This means add a, b, and c, and remove x, y, and z
+        List<String> modesToAdd = new ArrayList<>();
+        List<String> modesToRemove = new ArrayList<>();
+
+        // Now, let's split the input into a list of modes
+        // We want to handle a string like +ab-cd+ef-gh
+        // This means we add a, b, and remove c, d, and add e, f, and remove g, h
+        // So, we split on the + and - and then add the modes to the list
+        String[] splitInput = mode.split("(?=[+-])"); // will give us ["+ab", "-cd", "+ef", "-gh"]
+        for (String input : splitInput) {
+            // If the input is empty, skip it
+            if (input.isEmpty()) {
+                continue;
             }
-        } else if (mode.length() <= 1) {
-            return;
-        } else if (mode.charAt(0) == '+'){
-            Roles.UserMode to_add = Roles.UserMode.valueOf(String.valueOf(mode.charAt(1)));
-            to_add.assignMode(user);
-        } else if (mode.charAt(0) == '-') {
-            Roles.UserMode to_add = Roles.UserMode.valueOf(String.valueOf(mode.charAt(1)));
-            to_add.removeMode(user);
-        } else {
-            return;
+
+            // If the input starts with a +, add the modes
+            if (input.startsWith("+")) {
+                modesToAdd.addAll(Arrays.asList(input.substring(1).split("")));
+            }
+
+            // If the input starts with a -, remove the modes
+            if (input.startsWith("-")) {
+                modesToRemove.addAll(Arrays.asList(input.substring(1).split("")));
+            }
         }
+
+        // Now we can add the modes
+        for (String modeToAdd : modesToAdd) {
+            // Get the mode from the string
+            Roles.UserMode userMode = Roles.UserMode.valueOf(modeToAdd);
+
+            // Add the role
+            userMode.assignMode(user);
+        }
+
+        // Now we can remove the modes
+        for (String modeToRemove : modesToRemove) {
+            // Get the mode from the string
+            Roles.UserMode userMode = Roles.UserMode.valueOf(modeToRemove);
+
+            // Remove the role
+            userMode.removeMode(user);
+        }
+
         event.replyEmbeds(new EmbedBuilder()
             .setTitle("**User Mode Changed Successfully**")
-            .setDescription(user.getAsMention() + " has been given mode " + mode + " by " + event.getUser().getAsMention())
+            .setDescription(user.getAsMention() + " has modes updated to " + mode + " by " + event.getUser().getAsMention())
             .setColor(Color.GREEN)
             .build()).queue();
         AuditLogManager.logEntry(AuditLogManager.LogType.MODE_CHANGE, user.getUser(), event.getMember(), mode);
+    }
+
+    public void onAutoComplete(CommandAutoCompleteInteractionEvent event) {
+        // So we want to be fancy?
+
+        // Get all assignable user modes
+        Roles.UserMode[] modes = Roles.UserMode.values();
+
+        // Get the current input
+        String input = event.getOption("mode").getAsString();
+
+        // If the input is empty, we can just return
+        if (input.isEmpty()) {
+            event.replyChoices(new Command.Choice("Start Typing!", "n/a")).queue();
+            return;
+        }
+
+        // If the input doesn't start with + or -, we can just return
+        if (!input.startsWith("+") && !input.startsWith("-")) {
+            event.replyChoices(new Command.Choice("Invalid input! Start with + or -", "n/a")).queue();
+            return;
+        }
+
+        // Get only letters from the input
+        String[] split = input.replaceAll("[^a-zA-Z]", "").split("");
+        // Add the letters to the list
+        List<String> modesInput = new ArrayList<>(Arrays.asList(split));
+
+        // Now we can start suggesting!
+        List<Command.Choice> choices = new ArrayList<>();
+        // We're adding modes, so we need to suggest modes that aren't already added
+        for (Roles.UserMode mode : modes) {
+            // If the mode is already added, we can skip it
+            if (modesInput.contains(mode.name())) continue;
+
+            // If the mode can't be given, we can skip it
+            if (!mode.canGive()) continue;
+
+            // If we made it this far, we can add the mode to the list of choices. We need to handle our current input, tho
+            choices.add(new Command.Choice(input + mode.name(), input + mode.name()));
+        }
+
+        // Now we can reply with the choices
+        event.replyChoices(choices.toArray(new Command.Choice[0])).queue();
     }
 }
